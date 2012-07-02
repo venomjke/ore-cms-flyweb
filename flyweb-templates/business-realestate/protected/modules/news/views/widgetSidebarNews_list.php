7437916446d95b303aac31888638adc2358df9c9
alex.strigin <apstrigin@gmail.com>
2012-07-03 03:12:59 +0400

    update business template

--- flyweb-templates/business-realestate/protected/modules/news/views/widgetSidebarNews_list.php
+++ flyweb-templates/business-realestate/protected/modules/news/views/widgetSidebarNews_list.php
@@ -1,7 +1,7 @@
 <?php
 foreach ($news as $item){
 	?>
-	<a class="anons" href="111">
+	<a class="anons" href="<?php echo $item->getUrl(); ?>">
 	 	<h3><?php echo truncateText($item->title,7); ?></h3>
 		<p><?php echo truncateText($item->body,10); ?></p>
 	</a>
