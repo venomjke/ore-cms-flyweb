e74d0ccbf8d01483782022b9ab465b85fc041ac2
alex.strigin <apstrigin@gmail.com>
2012-07-07 22:44:52 +0400

    fix aparments

---
+++ flyweb-templates/beaty-realestate/protected/modules/apartments/helpers/apartmentsHelper.php
@@ -0,0 +1,76 @@
+<?php
+/**********************************************************************************************
+*                            CMS Open Real Estate
+*                              -----------------
+*	version				:	1.2.0
+*	copyright			:	(c) 2012 Monoray
+*	website				:	http://www.monoray.ru/
+*	contact us			:	http://www.monoray.ru/contact
+*
+* This file is part of CMS Open Real Estate
+*
+* Open Real Estate is free software. This work is licensed under a GNU GPL.
+* http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*
+* Open Real Estate is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* Without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+***********************************************************************************************/
+class apartmentsHelper {
+	public static function getApartments($limit = 10, $usePagination = 1, $all = 1, $criteria = null){
+		$pages = array();
+		$apartments = array();
+
+		Yii::app()->getModule('apartments');
+
+		if($criteria === null){
+			$criteria = new CDbCriteria;
+			if(!$all){
+				$criteria->condition = 'active=1';
+			}
+		}
+
+		$priceCurrency = (Yii::app()->language == 'ru' ? 'rur' : 'usd');
+
+		$sort = new CSort('Apartment');
+		$sort->attributes = array(
+			'price' => 'price_from_'.$priceCurrency,
+			'rating' => 'rating',
+		);
+		if(!$criteria->order){
+			$sort->defaultOrder = 't.sorter DESC';
+		}
+		$sort->applyOrder($criteria);
+
+		$sorterLinks = self::getSorterLinks($sort);
+		$criteria->addCondition('t.owner_id = 1 OR t.owner_active = 1');
+
+		if($usePagination){
+			$pages = new CPagination(Apartment::model()->count($criteria));
+			$pages->route = "/apartments/main/viewlist";
+			$pages->pageSize = $limit;
+			$pages->applyLimit($criteria);
+		}
+		else{
+			$criteria->limit = $limit;
+		}
+
+		$apartments = Apartment::model()
+			->cache(param('cachingTime', 1209600), Apartment::getImagesDependency())
+			->with(array('images'))
+			->findAll($criteria);
+		return array(
+			'pages' => $pages,
+			'apartments' => $apartments,
+			'sorterLinks' => $sorterLinks,
+		);
+	}
+
+	public static function getSorterLinks($sort){
+        $HtmlOption = array('onClick'=>'reloadApartmentList(this.href); return false;');
+		$return = array(
+			$sort->link('price', tt('Sorting by price', 'quicksearch'), $HtmlOption),
+			$sort->link('rating', tt('Sorting by rating', 'quicksearch'), $HtmlOption),
+		);
+		return $return;
+	}
+}
---
+++ flyweb-templates/beaty-realestate/protected/modules/apartments/views/widgetApartments_list2.php
@@ -0,0 +1,26 @@
+<div class="appartment_box" id="appartment_box">
+<?php
+if($apartments){
+	
+	foreach ($apartments as $item){
+		$this->renderPartial('widgetApartments_list_item', array(
+			'item' => $item,
+		));
+	}
+}
+?>
+</div>
+
+
+<?php
+if(!$apartments){
+	echo Yii::t('module_apartments','Apartments list is empty.');
+}
+
+if($pages){
+	// print_r($pages);
+	$this->widget('itemPaginator', array('pages' => $pages, 'header' => '', 'htmlOption'=>array('onClick'=>'reloadApartmentList(this.href); return false;')));
+}
+?>
+
+<script type="text/javascript"> jQuery('.ratingview > span > input').rating({'readOnly':true}); </script>
