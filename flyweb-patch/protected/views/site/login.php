cae8da59f9c55368f700b764c171c28f7d2c4080
alex.strigin <apstrigin@gmail.com>
2012-07-08 13:49:47 +0400

    formatted login

--- flyweb-patch/protected/views/site/login.php
+++ flyweb-patch/protected/views/site/login.php
@@ -17,28 +17,25 @@ $this->breadcrumbs=array(
 )); ?>
 
 	<p class="note"><?php echo Yii::t('common', 'Fields with <span class="required">*</span> are required.'); ?></p>
+		<?php echo $form->errorSummary($model); ?>
+	<table>
+		<tr> 
+			<td> 		<?php echo $form->labelEx($model,'username'); ?> </td>
+			<td>		<?php echo $form->textField($model,'username'); ?></td>
+		</tr>
+		<tr>
+			<td> 		<?php echo $form->labelEx($model,'password'); ?> </td>
+			<td>		<?php echo $form->passwordField($model,'password'); ?> </td>
+		</tr>
+		<tr>
+			<td>		<?php echo $form->label($model,'rememberMe'); ?>			</td>
+			<td>  		<?php echo $form->checkBox($model,'rememberMe'); ?> </td> 
+		</tr>
+		<tr>
+			<td colspan="2" class="buttons"> <?php echo CHtml::submitButton(Yii::t('common', 'Login')); ?> </td>
+		</tr>
+	</table>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-	</div>
-
-	<div class="row rememberMe">
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
-	</div>
-	
-	<div class="row buttons">
-		<?php echo CHtml::submitButton(Yii::t('common', 'Login')); ?>
-	</div>
 
 <?php $this->endWidget(); ?>
 </div><!-- form -->
